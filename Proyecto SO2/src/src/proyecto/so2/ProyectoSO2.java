870960ec5baefc616278b5a86e7f2c0f5b6c62aa
DaniT15 <122325895+DaniT15@users.noreply.github.com>
2024-10-27 16:59:18 -0400

    Fix commit anterior

---
+++ Proyecto SO2/src/src/proyecto/so2/ProyectoSO2.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto.so2;
+
+import Clases.Administrador;
+import Clases.ColasPrioridad;
+import Clases.IA;
+
+/**
+ *
+ * @author danie
+ */
+public class ProyectoSO2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws InterruptedException {
+        
+        ColasPrioridad colas = new ColasPrioridad();
+        
+        IA ia = new IA();
+        
+        Administrador admin = new Administrador();
+        
+        colas.personajesStartrek();
+        colas.personajesStarwars();
+        
+
+        admin.asignacion(colas);
+        
+        for(int i = 0; i < 2; i++){
+            admin.ejecucionPelea(colas, ia);
+        
+            colas.printColas();
+        }
+        
+        ia.cambiarTiempoCombate(1);
+        
+        for(int i = 0; i < 15; i++){
+            admin.ejecucionPelea(colas, ia);
+        
+            colas.printColas();
+        }
+        
+        
+       
+    }
+    
+}
